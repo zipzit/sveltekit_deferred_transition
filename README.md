eef43c2c90bf2f1f1648c86303b4888dd859de40
LB Corney <zipzit@yahoo.com>
2021-09-20 16:24:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# What is this?
+
+Thie is a simple SvelteKit sample of a deferred transitions sample.
+
+The original work was provided at:  https://svelte.dev/examples#deferred-transitions 
+
+This program originated as a skeleton only Sveltekit build.
+
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+    import DeferredTransitions from "$lib/specialTest/DeferredTransitions.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<DeferredTransitions/>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
